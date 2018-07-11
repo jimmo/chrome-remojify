05a96164d25dc2afab30ea504615c481801990e2
Jim Mussared <jim.mussared@gmail.com>
2018-07-11 20:12:53 +1000

    Initial version - Hangouts, GitHub, Slack

---
+++ content.js
@@ -0,0 +1,43 @@
+function replaceTextWithEmojiSpan(elem, s) {
+  const text = document.createTextNode(s);
+  let span = document.createElement('span');
+  span.style.fontFamily = 'emoji';
+  span.style.fontSize = '125%';
+  span.appendChild(text);
+  elem.replaceWith(span);
+}
+
+
+function replaceCodePointWithEmojiSpan(elem, hex) {
+  const ord = parseInt(hex, 16);
+  if (!isNaN(ord)) {
+    replaceTextWithEmojiSpan(elem, String.fromCodePoint(ord));
+  }
+}
+
+
+function fixEmoji() {
+  // Hangouts
+  for (const elem of document.querySelectorAll('span[data-emo], img[data-emo]')) {
+    const text = elem.dataset['emo'];
+    if (elem.parentNode.classList.contains('editable')) {
+      elem.replaceWith(document.createTextNode(text));
+    } else {
+      replaceTextWithEmojiSpan(elem, text);
+    }
+  }
+  // GitHub
+  for (const elem of document.querySelectorAll('g-emoji img')) {
+    const url = elem.parentNode.getAttribute('fallback-src').split('/');
+    const hex = url[url.length - 1].split('.')[0];
+    replaceCodePointWithEmojiSpan(elem.parentNode, hex);
+  }
+  // Slack
+  for (const elem of document.querySelectorAll('span.emoji-outer')) {
+    const hex = elem.dataset['codepoints'].split('-')[0];
+    replaceCodePointWithEmojiSpan(elem, hex);
+  }
+};
+
+const observer = new MutationObserver(fixEmoji);
+observer.observe(document.body, { 'childList': true, 'subtree': true });
